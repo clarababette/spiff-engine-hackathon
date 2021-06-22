9830a51c11e02f3af58a93075b7f24e70105f4b8
Amanda <amandamaarman19911@gmail.com>
2021-06-22 17:08:28 +0200

    added meals and digestion list

---
+++ js/data.js
@@ -0,0 +1,90 @@
+// const meals{
+//     const breakfast=["cereal","cereal","cereal","cereal","cereal","cereal"];
+//     const lunch=["Purity","mashed potatoes","butternut soup"];
+//     const snack=["teething biscuits", "boiled water"];
+//     const dinner=["mashed sweet potatoes","pureed apple and squash mixed", "butternut"];
+// }
+
+const meals = {
+    breskfast: ["cereal","porridge","milk"],
+    lunch: ["Purity","mashedPotatoes","butternutSoup"],
+    snack: ["teething biscuits", "boiled water","veggie straws","mango and carrot bear paws"],
+    dinner: ["mashed sweet potatoes","pureedApple and squash mixed", "butternut"]
+}
+
+const digestion = {
+    vomitting: {
+        milk: 5,
+        porridge:5,
+        milk: 3,
+        Purity: 4,
+        mashedPotatoes: 2,
+        butternutSoup: 1,
+
+
+    },
+    constipation:{
+        milk: 5,
+        porridge:5,
+        milk: 3,
+        Purity: 4,
+        mashedPotatoes: 2,
+        butternutSoup: 1,
+    },
+    diarrhoea:{
+        milk: 5,
+        porridge:5,
+        milk: 3,
+        Purity: 4,
+        mashedPotatoes: 2,
+        butternutSoup: 1,
+    }
+
+}
+
+const habits = {
+    
+}
+
+//each feed for new meal
+const feeds = {
+    currentDate-breakfast: {
+        time: '',
+
+        milk: 'ate most',
+
+    },
+
+}
+
+// const listIds = [breakfast,lunch,snack,supper];
+// const date = currentDate;
+// const time = currentTime;
+// const breakfastList=["cereal","porridge","milk"];
+// const lunchList=["Purity","mashed potatoes","butternut soup"];
+// const snackList=["teething biscuits", "boiled water","veggie straws","mango and carrot bear paws"];
+// const dinnerList=["mashed sweet potatoes","pureed apple and squash mixed", "butternut"]; 
+
+// // Create the object array
+// const feeds = id.map((id,index) => {
+//     return {
+//       id: id,
+//       breacky: breakfastList[index],
+//       lunch:lunchList[index],
+//       snack:snackList[index],
+//       supper:dinnerList[index]
+//     }
+//   });
+
+//   const meals = [
+//     {id:breakfast, food:"cereal", time: 17:00, date:25 June},
+//     {id:lunch, food: "purity"},
+//     {id:snack, food: "teething buscuits"},
+//     {id:supper, food: "butternut soup"},
+    
+// ];
+
+// //TRENDS
+// const trends = {
+    
+// }
