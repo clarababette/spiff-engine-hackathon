e05205f499b74ebdf31862d7fcbc66cd50aa8394
Amanda <amandamaarman19911@gmail.com>
2021-06-23 12:44:25 +0200

    added functionality on added meals

---
+++ js/data.js
@@ -0,0 +1,68 @@
+const meals = {
+    breskfast: ['cereal', 'porridge', 'milk'],
+    lunch: ['Purity', 'mashed potatoes', 'butternut soup'],
+    snack: ['teething biscuits', 'boiled water', 'veggie straws', 'mango and carrot bear paws'],
+    dinner: ['mashed sweet potatoes', 'pureed apple and squash', 'butternut'],
+  };
+  
+  const digestion = {
+    vomitting: {},
+    constipation: {},
+    diarrhoea: {},
+  };
+  
+  const habits = {
+    "didn't eat any": {},
+    'ate some': {},
+    'ate most': {},
+    'ate it all': {},
+    'ate it all and wanted': {},
+  };
+  
+  const newFeed = {
+    time: '',
+    'fed by': '',
+    contained: '',
+  };
+  
+  // //each feed for new meal
+  // const feeds = {
+  //     //BREAKFAST
+  //     breakfast: {
+  //     milk: 'ate most',
+  //     date: 2021-06-25,
+  //     time: '9am',
+  //     },
+  //     breakfast: {
+  //         cereal: 'ate most',
+  //         date: 2021-06-25,
+  //         time: '6am',
+  //     },
+  
+  //     breakfast: {
+  //         fruitYoghurt: 'portion',
+  //         date: 2021-06-25,
+  //         time: '8pm',
+  //     },
+  
+  //     lunch: {
+  //         butternutSoup: 'ate most',
+  //         date: 2021-06-25,
+  //         time: '3pm',
+  //     },
+  //     supper: {
+  //         date: 2021-06-25,
+  //         time: '9pm',
+  //         milk: 'ate most',
+  //     },
+  //     supper: {
+  //         mashedPotatoes: 'ate most',
+  //         date: 2021-06-25,
+  //         time: '7:30pm',
+  //     },
+  //     supper: {
+  //         butternutSoup: 'ate most',
+  //         date: 2021-06-25,
+  //         time: '7:30pm',
+  //     },
+  // }
--- js/new-feed-DOM.js
+++ js/new-feed-DOM.js
@@ -1,3 +1,28 @@
 const buttonElement = document.querySelector(".btn");
+const successfullyElem = document.querySelector(".successfully");
+const displayAddedMealElem = document.querySelector(".displayAddedMeal");
 
 // buttonElement.addEventListener('click',function()
+
+//instance for factory
+var tracker = feedsTracker();
+const storageList = [];
+if(localStorage['feedsList']) {
+	storageList = JSON.parse(localStorage.getItem('feedsList'));
+}
+
+function addMeal(){
+	var eachFeed = document.createElement('Li');
+	var eachMeal = checkedMeal.value
+
+    eachFeed.innerHTML = tracker.setMeal(eachMeal);
+
+
+    displayAddedMealElem.appendChild(eachFeed);
+	successfullyElem.innerHTML = 'seccessfully added a meal'
+
+    let key = feedsTracker.getMeal();
+	localStorage.setItem('feedsList', JSON.stringify(key));
+
+}
+
