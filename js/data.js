0b1f51096197057b22fe82fe94e78fb86aed3d7f
clarababette <clara_coetzee@zoho.com>
2021-06-23 10:53:03 +0200

    Apply suggestions from code review

--- js/data.js
+++ js/data.js
@@ -1,8 +1,8 @@
 const meals = {
     breskfast: ["cereal","porridge","milk"],
-    lunch: ["Purity","mashedPotatoes","butternutSoup"],
+    lunch: ["Purity","mashed potatoes","butternut soup"],
     snack: ["teething biscuits", "boiled water","veggie straws","mango and carrot bear paws"],
-    dinner: ["mashed sweet potatoes","pureedApple and squash mixed", "butternut"]
+    dinner: ["mashed sweet potatoes","pureed apple and squash", "butternut"]
 }
 
 const digestion = {
