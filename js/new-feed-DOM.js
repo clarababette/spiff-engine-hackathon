3db92ecd29e5ae544ef627e8b365756df7ef8dcf
0ngezwa-felele <ongezwa@codex.com>
2021-06-23 12:45:17 +0200

    added changes on js

--- js/new-feed-DOM.js
+++ js/new-feed-DOM.js
@@ -1,3 +1,18 @@
-const buttonElement = document.querySelector(".btn");
+const addButtonElement = document.querySelector(".btn");
+const submitButtonElement = document.querySelector(".subBtn")
+const mealElement = document.querySelector(".meal")
 
-// buttonElement.addEventListener('click',function()
+
+mealElement.addEventListener('change',function(){
+    mealElement.querySelector("meal").innerHTML = ""
+})
+
+addButtonElement.addEventListener('click',function(){
+    addButtonElement.querySelector("btn").innerHTML = ""
+
+})
+
+    submitButtonElement.addEventListener('click',function(){
+
+    })
+    
